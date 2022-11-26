9ca7521f358b88483c6537cf833906d156d787e6
tangpeng <tangpeng@ccclubs.com>
2022-11-26 17:16:05 +0800

    添加用户忘记密码、答案

---
+++ src/main/java/com/mymall/service/IUserService.java
@@ -0,0 +1,13 @@
+package com.mymall.service;
+
+import com.mymall.common.ServerResponse;
+import com.mymall.pojo.User;
+
+public interface IUserService {
+    ServerResponse<User> login(String username, String password);
+    ServerResponse<String> register(User user);
+    ServerResponse<String> checkValid(String str, String type);
+    ServerResponse<String> selectQuestion(String username);
+    ServerResponse<String> checkAnswer (String username, String question, String answer);
+
+}
